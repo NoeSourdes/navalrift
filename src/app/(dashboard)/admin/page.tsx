c347c21ce9628abefd4d5b28ad0eefac38d3eba4
NoeSourdes <sourdesnoe19@gmail.com>
2024-02-24 10:53:36 +0100

    Refactor admin page layout

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -19,8 +19,8 @@ export default function Page() {
 
   return (
     <div className="h-full w-full flex flex-col gap-6">
-      <div className="relative h-full w-full bg-blue-800/75 rounded-xl flex max-md:flex-col items-center justify-center gap-6 p-6">
-        <div className="absolute sm:top-12 top-8 left-1/2 text-sm text-gray-300 -translate-x-1/2 h-15 px-3 py-2 rounded-full bg-blue-900 border-2 border-blue-800 z-20 shadow-xl">
+      <div className="relative h-full w-full bg-blue-800/75 rounded-xl flex max-md:flex-col items-center lg:justify-center gap-6 p-6 overflow-y-scroll">
+        <div className="absolute sm:top-12 top-5 left-1/2 text-sm text-gray-300 -translate-x-1/2 h-15 px-3 py-2 rounded-full bg-blue-900 border-2 border-blue-800 z-20 shadow-xl">
           Faites votre choix
         </div>
         <div
@@ -29,7 +29,7 @@ export default function Page() {
             onOpen();
           }}
           onMouseEnter={() => playHover()}
-          className="h-full w-full bg-blue-900  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center rounded-lg overflow-hidden cursor-pointer hover:scale-[1.02] transition-all z-10"
+          className="h-full w-full bg-blue-900  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center rounded-lg overflow-hidden cursor-pointer hover:scale-[1.02] transition-all z-10 min-h-[300px]"
         >
           <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-blue-900 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] "></div>
           <div className="max-w-2xl mx-auto p-4">
@@ -46,7 +46,7 @@ export default function Page() {
         </div>
         <div
           onMouseEnter={() => playHover()}
-          className="h-full w-full  bg-blue-900  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center rounded-lg overflow-hidden cursor-pointer hover:scale-[1.02] transition-all z-10"
+          className="h-full w-full  bg-blue-900  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center rounded-lg overflow-hidden cursor-pointer hover:scale-[1.02] transition-all z-10 min-h-[300px]"
         >
           <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-blue-900 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
           <div className="max-w-2xl mx-auto p-4">
