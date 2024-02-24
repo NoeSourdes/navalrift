37276fef648a2461de0fcb8aef38de8a39e50199
NoeSourdes <sourdesnoe19@gmail.com>
2024-02-24 10:39:18 +0100

    Add modal and button functionality to admin page

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,15 +1,35 @@
 "use client";
 
 import { useButtonSounds } from "@/app/actions/sound/sound";
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  useDisclosure,
+} from "@nextui-org/react";
+import { useState } from "react";
 
 export default function Page() {
   const { playHover, play } = useButtonSounds();
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const [sentance, setSentance] = useState("");
+
   return (
     <div className="h-full w-full flex flex-col gap-6">
-      <div className="h-full w-full bg-blue-800/75 rounded-xl flex max-md:flex-col items-center justify-center gap-6 p-6">
+      <div className="relative h-full w-full bg-blue-800/75 rounded-xl flex max-md:flex-col items-center justify-center gap-6 p-6">
+        <div className="absolute sm:top-12 top-8 left-1/2 text-sm text-gray-300 -translate-x-1/2 h-15 px-3 py-2 rounded-full bg-blue-900 border-2 border-blue-800 z-20 shadow-xl">
+          Faites votre choix
+        </div>
         <div
+          onClick={() => {
+            setSentance("Jouer contre une IA");
+            onOpen();
+          }}
           onMouseEnter={() => playHover()}
-          className="h-full w-full bg-blue-900  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center rounded-lg overflow-hidden cursor-pointer hover:scale-[1.02] transition-all"
+          className="h-full w-full bg-blue-900  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center rounded-lg overflow-hidden cursor-pointer hover:scale-[1.02] transition-all z-10"
         >
           <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-blue-900 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] "></div>
           <div className="max-w-2xl mx-auto p-4">
@@ -26,7 +46,7 @@ export default function Page() {
         </div>
         <div
           onMouseEnter={() => playHover()}
-          className="h-full w-full  bg-blue-900  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center rounded-lg overflow-hidden cursor-pointer hover:scale-[1.02] transition-all"
+          className="h-full w-full  bg-blue-900  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center rounded-lg overflow-hidden cursor-pointer hover:scale-[1.02] transition-all z-10"
         >
           <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-blue-900 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
           <div className="max-w-2xl mx-auto p-4">
@@ -43,6 +63,60 @@ export default function Page() {
           </div>
         </div>
       </div>
+      <Modal
+        backdrop="opaque"
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        motionProps={{
+          variants: {
+            enter: {
+              y: 0,
+              opacity: 1,
+              transition: {
+                duration: 0.3,
+                ease: "easeOut",
+              },
+            },
+            exit: {
+              y: -20,
+              opacity: 0,
+              transition: {
+                duration: 0.2,
+                ease: "easeIn",
+              },
+            },
+          },
+        }}
+      >
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                <h3>Commencer une partie</h3>
+              </ModalHeader>
+              <ModalBody>
+                <p>{sentance}</p>
+              </ModalBody>
+              <ModalFooter>
+                <Button
+                  onMouseEnter={() => playHover()}
+                  color="danger"
+                  variant="ghost"
+                  onPress={() => {
+                    onClose();
+                    play();
+                  }}
+                >
+                  Annuler
+                </Button>
+                <Button color="primary" onMouseEnter={() => playHover()}>
+                  Commencer
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
     </div>
   );
 }
