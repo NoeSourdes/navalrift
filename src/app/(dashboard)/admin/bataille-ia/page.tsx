2363fa122ae21a58b472a60d5614dbf29b1e7f36
NoeSourdes <sourdesnoe19@gmail.com>
2024-03-24 11:14:31 +0100

    Refactor DropZone component and import in BatailleIa page

--- src/app/(dashboard)/admin/bataille-ia/page.tsx
+++ src/app/(dashboard)/admin/bataille-ia/page.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import { Button } from "@nextui-org/react";
-import Grid from "../components/bataille/DropZone";
-import { Ship } from "../components/bataille/Ship";
+import { Grid } from "../components/bataille/DropZone";
 
 export default function BatailleIa() {
   return (
@@ -17,25 +16,25 @@ export default function BatailleIa() {
       >
         Retour
       </Button>
-      <div className="h-full w-full  bg-blue-900  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center rounded-lg overflow-hidden p-2 ">
-        <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-blue-900 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+      <div className="h-full w-full  bg-blue-900  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center rounded-lg overflow-y-auto p-2 max-md:pt-28">
+        <div className="absolute pointer-events-none inset-0 -bottom-10 flex items-center justify-center bg-blue-900 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
         <div className="relative flex flex-col items-center justify-center gap-10 z-20">
           <div className="text-center space-y-3">
-            <h1 className="text-4xl font-bold">
+            <h1 className="text-4xl font-bold max-md:text-3xl">
               Bataile contre <span className="text-primary">l&apos;IA</span>
             </h1>
-            <h3 className="text-2xl font-bold">
+            <h3 className="text-2xl font-bold max-md:text-xl">
               Choisissez comment vous voulez placez vos{" "}
               <span className="text-primary">bateaux</span> dans la grille
             </h3>
           </div>
-          <div className="flex items-center gap-10">
-            <div className="w-96 h-96 border-2 border-border bg-black/15 rounded-xl flex justify-center items-center gap-10">
-              <Ship />
-            </div>
+          <div className="flex items-center gap-5 flex-col">
             <div className="w-96 h-96 border-border bg-blue-800/75 rounded-xl overflow-hidden">
               <Grid />
             </div>
+            <Button color="primary" variant="faded">
+              Regénérer la grille
+            </Button>
           </div>
           <Button color="primary" size="lg">
             Affronter l&apos;IA
