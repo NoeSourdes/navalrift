893d6662d0680c01524e85d762735f0e9504f79e
NoeSourdes <sourdesnoe19@gmail.com>
2024-03-24 11:26:57 +0100

    Refactor BatailleIa page layout and styling

--- src/app/(dashboard)/admin/bataille-ia/page.tsx
+++ src/app/(dashboard)/admin/bataille-ia/page.tsx
@@ -16,10 +16,10 @@ export default function BatailleIa() {
       >
         Retour
       </Button>
-      <div className="h-full w-full  bg-blue-900  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex justify-center rounded-lg overflow-hidden p-2 max-md:pt-16">
+      <div className="h-full w-full  bg-blue-900  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex justify-center md:items-center rounded-lg overflow-hidden p-2 max-md:pt-16">
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-blue-900 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-        <div className="relative flex flex-col gap-10 z-20 overflow-y-auto py-10">
-          <div className="text-center space-y-3">
+        <div className="relative flex flex-col gap-8 z-20 overflow-y-auto py-8">
+          <div className="text-center space-y-2">
             <h1 className="text-4xl font-bold max-md:text-3xl">
               Bataile contre <span className="text-primary">l&apos;IA</span>
             </h1>
@@ -28,11 +28,11 @@ export default function BatailleIa() {
               <span className="text-primary">bateaux</span> dans la grille
             </h3>
           </div>
-          <div className="flex items-center gap-5 flex-col">
+          <div className="flex items-center gap-6 flex-col">
             <div className="w-96 h-96 border-border bg-blue-800/75 rounded-xl overflow-hidden max-sm:w-80 max-sm:h-80">
               <Grid />
             </div>
-            <div className="flex flex-col space-y-10">
+            <div className="flex flex-col space-y-8">
               <Button color="primary" variant="faded">
                 Regénérer la grille
               </Button>
